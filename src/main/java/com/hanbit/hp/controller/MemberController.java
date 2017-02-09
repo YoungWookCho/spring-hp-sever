0c8616687e230e8a49bac65a5d848cdcfb7345e8
impact0823@naver.com <young1596>
2017-02-09 14:20:42 +0900

    한빛 플레이트 로그인, 회원가입

--- src/main/java/com/hanbit/hp/controller/MemberController.java
+++ src/main/java/com/hanbit/hp/controller/MemberController.java
@@ -3,6 +3,9 @@ package com.hanbit.hp.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,35 +15,121 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hanbit.hp.service.MemberService;
-// 커트롤러 설정
+
 @Controller
 public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
-	
-	//주소를 매핑해줌
+
 	@RequestMapping(value="/api2/member/signup", method=RequestMethod.POST)
-	//주소를 받는 부분
 	@ResponseBody
-	//@RequestParam 으로 받아야 하는데 유저아이디 ""해주는 이유는 자바 7에서 해줘야함
-	public Map signup (@RequestParam("userId") String userId, 
+	public Map signup(@RequestParam("userId") String userId,
 			@RequestParam("userPw") String userPw) {
-			
-		System.out.println(userId+" / "+userPw);
 		
 		if (StringUtils.isBlank(userId)) {
-			throw new RuntimeException("아이디가 잘못되었습니다");
+			throw new RuntimeException("아이디가 잘못 입력되었습니다.");
 		}
 		else if (StringUtils.isBlank(userPw)) {
-			throw new RuntimeException("비밀번호가 잘못되었습니다");
+			throw new RuntimeException("비밀번호가 잘못 입력되었습니다.");
 		}
 		
-		String uid =memberService.addMember(userId, userPw);
-
+		String uid = memberService.addMember(userId, userPw);
+		
 		Map result = new HashMap();
 		result.put("result", "ok");
 		result.put("uid", uid);
+		
 		return result;
 	}
+	
+	@RequestMapping(value="/api2/member/signin", method=RequestMethod.POST)
+	@ResponseBody
+	public Map signin(@RequestParam("userId") String userId,
+			@RequestParam("userPw") String userPw,
+			HttpSession session) {
+		
+		try {
+			if (!memberService.isValidMember(userId, userPw)) {
+				throw new RuntimeException("패스워드가 다릅니다.");
+			}
+		}
+		catch (NullPointerException e) {
+			throw new RuntimeException("가입되지 않은 사용자입니다.");
+		}
+		
+		String uid = memberService.getUid(userId);
+		
+		session.setAttribute("signedIn", true);
+		session.setAttribute("uid", uid);
+		session.setAttribute("userId", userId);
+		
+		Map result = new HashMap();
+		result.put("result", "ok");
+		
+		return result;
+	}
+	
+	@RequestMapping("/api2/member/signedin")
+	@ResponseBody
+	public Map signedin(HttpSession session) {
+		Map result = new HashMap();
+		String signedIn = "no";
+		
+		if (session.getAttribute("signedIn") != null &&
+				(Boolean) session.getAttribute("signedIn")) {
+			signedIn = "yes";
+			
+			result.put("userId", session.getAttribute("userId"));
+		}
+		
+		result.put("result", signedIn);
+		
+		return result;
+	}
+	
+	@RequestMapping(value="/api2/member/update", method=RequestMethod.POST)
+	@ResponseBody
+	public Map update(@RequestParam("userPw") String userPw,
+			HttpSession session) {
+		
+		String uid = (String) session.getAttribute("uid");
+		
+		if (StringUtils.isBlank(uid)) {
+			throw new RuntimeException("로그인이 필요합니다.");
+		}
+		
+		memberService.modifyMember(uid, userPw);
+		
+		Map result = new HashMap();
+		result.put("result", "ok");
+		
+		return result;
+	}
+	
+	@RequestMapping("/api2/member/signout")
+	@ResponseBody
+	public Map signout(HttpSession session) {
+		
+		session.invalidate();
+		
+		Map result = new HashMap();
+		result.put("result", "ok");
+		
+		return result;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/hanbit/hp/service/MemberService.java
+++ src/main/java/com/hanbit/hp/service/MemberService.java
@@ -1,28 +1,66 @@
 package com.hanbit.hp.service;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.hanbit.hp.dao.MemberDAO;
 
 @Service
 public class MemberService {
+
+	private static final String SECRET_KEY = "hanbit";
+	private PasswordEncoder passwordEncoder = new StandardPasswordEncoder(SECRET_KEY);
 	
 	@Autowired
 	private MemberDAO memberDAO;
 	
 	private String generateKey(String prefix) {
-		String key = prefix +StringUtils.leftPad(
+		String key = prefix + StringUtils.leftPad(
 				String.valueOf(System.nanoTime()), 30, "0");
-		key+=StringUtils.leftPad(String.valueOf((int)(Math.random()*1000) % 100),2,"0");
+		
+		key += StringUtils.leftPad(
+				String.valueOf((int) (Math.random() * 1000) % 100), 2, "0");
+		
 		return key;
 	}
 	
 	public String addMember(String userId, String userPw) {
-		String uid =generateKey("UID");
-		memberDAO.insertMember(uid, userId, userPw);
-		return uid;	
+		String uid = generateKey("UID");
+		String encryptedUserPw = passwordEncoder.encode(userPw);
+		
+		memberDAO.insertMember(uid, userId, encryptedUserPw);
+		
+		return uid;
 	}
+
+	public boolean isValidMember(String userId, String userPw) {
+		String encryptedUserPw = memberDAO.selectUserPw(userId);
+		
+		return passwordEncoder.matches(userPw, encryptedUserPw);
+	}
+
+	public void modifyMember(String uid, String userPw) {
+		String encryptedUserPw = passwordEncoder.encode(userPw);
+		
+		memberDAO.updateMember(uid, encryptedUserPw);
+	}
+
+	public String getUid(String userId) {
+		return memberDAO.selectUid(userId);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
